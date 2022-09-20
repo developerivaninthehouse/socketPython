9e2d93206052a143465f8edb29fab765d6f26bcb
developerivaninthehouse <106269746+developerivaninthehouse@users.noreply.github.com>
2022-09-20 16:28:36 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -45,7 +45,7 @@ def webServer(port=13331):
       #Send the content of the requested file to the client
       for i in f: #for line in file
         #Fill in start - send your html file contents #Fill in end 
-        connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send(outputdata[i])
 
 
       connectionSocket.close() #closing the connection socket
